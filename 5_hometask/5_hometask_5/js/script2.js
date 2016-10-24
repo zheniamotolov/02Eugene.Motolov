bec1ad2720601db34d819ce06ad6126ce07af2fb
Eugene <zheniamotolov@gmail.com>
2016-10-24 22:31:24 +0300

    Update script2.js

--- 5_hometask/5_hometask_5/js/script2.js
+++ 5_hometask/5_hometask_5/js/script2.js
@@ -3,27 +3,27 @@
 var countries = [
     {
         name: "Belarus",
-        flag: "../img/belarus.png",
+        flag: "img/belarus.png",
         Code: "+375",
         population: ">1.000.000",
         area: 207595
     },
     {
         name: "USA",
-        flag: "../img/usa.png",
+        flag: "img/usa.png",
         Code: "+1",
         population: "319.000.000",
         area: 9834000
     }, {
         name: "GreatBriatain",
-        flag: "../img/greatbritain.png",
+        flag: "img/greatbritain.png",
         Code: "+44",
         population: ">64.000.000",
         area: 243610
     },
     {
         name: "Austria",
-        flag: "../img/austria.png",
+        flag: "img/austria.png",
         Code: "+43",
         population: "8.472.000",
         area: 83879
@@ -32,9 +32,13 @@ var countries = [
 var headers = ["Name of country", "code", "population", "area", "flag"];
 function create_table() {
     var table = document.createElement("table");
-    document.body.appendChild(table);
+
     var tr = document.createElement("tr");
-    table.appendChild(tr);
+    var tbody = document.createElement("tbody");
+    //второй вариант создавать tbody динамически
+    table.appendChild(tbody);
+    document.body.appendChild(table);
+    tbody.appendChild(tr);
     headers.forEach(function (item) {
         var elem_of_header = document.createElement("th");
         tr.appendChild(elem_of_header);
@@ -46,16 +50,30 @@ function create_table() {
 function create_element(item) {
 //console.log(item.area);
     var tr = document.createElement("tr");
-    //var tbody = document.getElementsByTagName("tbody")[0];
-    var table=document.body.children[0];
-    table.appendChild(tr);
-    for (var key in item) {
-        var td = create_element("td");
-        td.innerHTML = key;
-        tr.appendChild(td);
-        //td.innerHTML
-    }
 
+    var tbody = document.getElementsByTagName("tbody")[0];
+    ///при условии что tbody создался автоматически
+    //
+    //var table=document.body.children[0];
+    tbody.appendChild(tr);
+    for (var key in item) {
+        if (!(key === "flag")) {
+            var td = document.createElement("td");
+            td.innerHTML = item[key];
+            tr.appendChild(td);
+        }
+        else {
+            var td = document.createElement("td");
+
+            var img = document.createElement("img");
+            img.src = item[key];
+            td.appendChild(img);
+            tr.appendChild(td);
+
+        }
+        //td.innerHTML
+        // }}
+    }
 }
 
 create_table();
