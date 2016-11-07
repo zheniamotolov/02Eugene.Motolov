842b253bcb7d27d10ac89272357cac60e021b1ad
Eugene Motolov <zheniamotolov@gmail.com>
2016-11-07 12:56:37 +0300

    New commit

--- api/source/javascript/create_element.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-function create_element(value) {
-    var elem = document.createElement("div");
-    elem.setAttribute("id", value);
-    var container = document.getElementsByClassName("container-fluid")[0];
-    container.appendChild(elem);
-
-}
